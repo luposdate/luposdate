e66cd4353cf5228241d7a9b99600fc3a6bb57aa3
luposdate <luposdate@ifis.uni-luebeck.de>
2012-11-29 13:13:02 +0100

    new compression classes

---
+++ compression/src/main/java/lupos/compression/bitstream/BitInputStream.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2012, Institute of Information Systems (Sven Groppe), University of Luebeck
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+ * following conditions are met:
+ *
+ * 	- Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+ * 	  disclaimer.
+ * 	- Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
+ * 	  following disclaimer in the documentation and/or other materials provided with the distribution.
+ * 	- Neither the name of the University of Luebeck nor the names of its contributors may be used to endorse or promote
+ * 	  products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package lupos.compression.bitstream;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class BitInputStream {
+
+	protected final InputStream in;
+
+	// the current position in the byte is initially set to 8 such that right the first time a new byte is read from the underlying input stream
+	protected int currentPosInByte = 8;
+	
+	protected int currentByte;
+	
+	public BitInputStream(final InputStream in){
+		this.in = in;
+	}
+	
+	public boolean readBit() throws IOException{
+		if(this.currentPosInByte>7){
+			this.currentByte = this.in.read();
+			this.currentPosInByte=0;
+		}
+		boolean result = (this.currentByte % 2) == 1; 
+		this.currentPosInByte++;
+		// prepare to read next bit by division through 2 (implemented by fast shifting bit positions)
+		this.currentByte >>= 1;
+		return result;
+	}
+	
+	public void close() throws IOException{
+		this.in.close();
+	}
+	
+}
---
+++ compression/src/main/java/lupos/compression/bitstream/BitOutputStream.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2012, Institute of Information Systems (Sven Groppe), University of Luebeck
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+ * following conditions are met:
+ *
+ * 	- Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+ * 	  disclaimer.
+ * 	- Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
+ * 	  following disclaimer in the documentation and/or other materials provided with the distribution.
+ * 	- Neither the name of the University of Luebeck nor the names of its contributors may be used to endorse or promote
+ * 	  products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package lupos.compression.bitstream;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class BitOutputStream {
+	
+	protected final OutputStream out;
+	
+	protected int currentByte = 0;
+	protected int currentBitValue = 1;
+
+	public BitOutputStream(final OutputStream out){
+		this.out = out;
+	}
+	
+	public void write(boolean b) throws IOException{
+		if(b){
+			this.currentByte += this.currentBitValue;
+		}
+		this.currentBitValue <<= 1;
+		if(this.currentBitValue == 256){
+			this.out.write(this.currentByte);
+			this.currentByte = 0;
+			this.currentBitValue = 1;
+		}
+	}
+}
