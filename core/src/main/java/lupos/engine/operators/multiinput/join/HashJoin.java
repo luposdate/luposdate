ccc9fef8cbe92f60029622020bf143c0c364623b
luposdate <luposdate@ifis.uni-luebeck.de>
2013-07-09 12:14:30 +0200

    Using ArrayList instead of LinkedList in Join to be memory-efficient

--- core/src/main/java/lupos/engine/operators/multiinput/join/HashJoin.java
+++ core/src/main/java/lupos/engine/operators/multiinput/join/HashJoin.java
@@ -23,6 +23,7 @@
  */
 package lupos.engine.operators.multiinput.join;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.LinkedList;
@@ -221,7 +222,7 @@ public class HashJoin extends Join {
 			final LeafNodeInPartitionTree smaller = (LeafNodeInPartitionTree) rootSmaller;
 			final Collection<Bindings> cb;
 			if (smaller.partition.size() <= LeafNodeInPartitionTree.maxNumberEntries) {
-				cb = new LinkedList<Bindings>();
+				cb = new ArrayList<Bindings>(smaller.partition.size());
 				// load smaller partition into main memory!
 				for (final Bindings b : smaller.partition) {
 					cb.add(b);
@@ -259,10 +260,15 @@ public class HashJoin extends Join {
 			}
 
 			final LeafNodeInPartitionTree smaller = (LeafNodeInPartitionTree) rootSmaller;
-			// load smaller partition into main memory!
-			final Collection<Bindings> cb = new LinkedList<Bindings>();
-			for (final Bindings b : smaller.partition) {
-				cb.add(b);
+			final Collection<Bindings> cb;
+			if (smaller.partition.size() <= LeafNodeInPartitionTree.maxNumberEntries) {
+				cb = new ArrayList<Bindings>(smaller.partition.size());
+				// load smaller partition into main memory!
+				for (final Bindings b : smaller.partition) {
+					cb.add(b);
+				}
+			} else {
+				cb = smaller.partition.getCollection();
 			}
 
 			for (final Bindings b1 : ((LeafNodeInPartitionTree) rootLarger).partition) {
