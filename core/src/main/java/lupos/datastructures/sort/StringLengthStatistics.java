c43a4ac7ac02f6efb805222c6b540297f4d722f8
luposdate <luposdate@ifis.uni-luebeck.de>
2015-05-08 10:51:30 +0200

    new class to compute string length statistics on large data

---
+++ core/src/main/java/lupos/datastructures/sort/StringLengthStatistics.java
@@ -0,0 +1,116 @@
+package lupos.datastructures.sort;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Collection;
+
+import lupos.datastructures.parallel.BoundedBuffer;
+import lupos.datastructures.smallerinmemorylargerondisk.PagedCollection;
+import lupos.datastructures.sort.helper.DataToBoundedBuffer;
+
+
+public class StringLengthStatistics {
+	public static void main(final String[] args) throws Exception{
+		System.out.println("Determining string length statistics of a large collection of Strings or RDF terms of large RDF data...");
+		if(args.length<2){
+			System.out.println(StringLengthStatistics.getHelpText());
+			return;
+		}
+		final BoundedBuffer<String> buffer = new BoundedBuffer<String>();
+		// read in and parse the data...
+		DataToBoundedBuffer.dataToBoundedBuffer(new BufferedInputStream(new FileInputStream(args[0])), args[1], buffer);
+
+		final PagedCollection<Integer> lengths = new PagedCollection<Integer>(Integer.class);
+		final StringLengthAdder stringLengthAdder = new StringLengthAdder(buffer, lengths);
+
+		// signal that all the data is parsed (and nothing will be put into the buffer any more)
+		buffer.endOfData();
+
+		// wait for threads to finish generating string lengths...
+		try {
+			stringLengthAdder.join();
+		} catch (final InterruptedException e) {
+			System.err.println(e);
+			e.printStackTrace();
+		}
+
+		final int length = lengths.size();
+		System.out.println("Number of strings           : "+length+"\n");
+		// now compute statistics
+		final BigDecimal[] terms = StringLengthStatistics.computeInnerTerm(lengths);
+		System.out.println("Average string length sizes : "+terms[0]+"\n");
+		System.out.println("Sample Standard Deviation   : "+computeSampleStandardDeviation(terms[1], length));
+		System.out.println("StandardDeviationOfTheSample: "+computeStandardDeviationOfTheSample(terms[1], length)+"\n");
+
+		int min = Integer.MAX_VALUE;
+		int max = Integer.MIN_VALUE;
+		for (final Integer l : lengths) {
+			if(l<min){
+				min = l;
+			}
+			if(l>max){
+				max = l;
+			}
+		}
+		System.out.println("Minimum string length size  : "+min);
+		System.out.println("Maximum string length size  : "+max+"\n");
+		lengths.release();
+	}
+
+	public static double computeSampleStandardDeviation(final BigDecimal innerTerm, final int length){
+		return Math.sqrt(innerTerm.divide(BigDecimal.valueOf(length-1)).doubleValue());
+	}
+
+	public static double computeStandardDeviationOfTheSample(final BigDecimal innerTerm, final int length) {
+		return Math.sqrt(innerTerm.divide(BigDecimal.valueOf(length)).doubleValue());
+	}
+
+	public static BigDecimal[] computeInnerTerm(final Collection<Integer> lengths) {
+		BigInteger sum = BigInteger.ZERO;
+		for (final Integer l : lengths) {
+			sum = sum.add(BigInteger.valueOf(l));
+		}
+		BigDecimal mean = new BigDecimal(sum);
+		mean = mean.divide(BigDecimal.valueOf(lengths.size()));
+		final BigDecimal innerTerm = BigDecimal.ZERO;
+		for (final Integer l : lengths) {
+			final BigDecimal diff = BigDecimal.valueOf(l).subtract(mean);
+			innerTerm.add(diff.multiply(diff));
+		}
+		return new BigDecimal[]{mean, innerTerm};
+	}
+
+	private static String getHelpText() {
+		String result = "Call StringLengthStatistics in the following way:\n\njava lupos.datastructures.sort.StringLengthStatistics DATAFILE FORMAT\n\n";
+		result += "DATAFILE contains the file with data (containing strings or RDF data)\n";
+		result += "FORMAT can be STRING for a large collection of strings in one file, MULTIPLESTRING for a list of files containing strings to be read, BZIP2STRING and MULTIPLEBZIP2STRING for analogous, but BZIP2 compressed files, or an RDF format like N3\n";
+		return result;
+	}
+
+	public static class StringLengthAdder extends Thread {
+		private final BoundedBuffer<String> buffer;
+		private final Collection<Integer> lengths;
+		public StringLengthAdder(final BoundedBuffer<String> buffer, final Collection<Integer> lengths){
+			this.buffer = buffer;
+			this.lengths = lengths;
+		}
+
+		@Override
+		public void run(){
+			try {
+				while(true){
+					final String item = this.buffer.get();
+					if(item==null){
+						break;
+					}
+					this.lengths.add(item.length());
+				}
+			} catch (final InterruptedException e) {
+				System.err.println(e);
+				e.printStackTrace();
+			}
+		}
+	}
+}
